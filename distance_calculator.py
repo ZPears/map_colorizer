7ce8dd83d5cc4a8090b30c3c8d4706b199c1a5df
Zachary Pearson <zachary.p.pearson@gmail.com>
2016-03-28 22:09:32 -0400

    starting function in distance calculator to find nearest non-white pixel

---
+++ distance_calculator.py
@@ -0,0 +1,5 @@
+from scipy import misc
+import tkFileDialog as tk
+import pandas as pd
+
+def find_nearest_road(image_df, row_index, col_index, destination):
--- map_colorer.py
+++ map_colorer.py
@@ -1,6 +1,8 @@
 from scipy import misc
 import tkFileDialog as tk
 import pandas as pd
+import numpy as np
+import distance_calculator as dc
 
 def main():
 	print('Choose an image to analyze.')
@@ -10,11 +12,18 @@ def main():
 
 	image_df = build_image_df(input_map)
 
+	destination_coords = [ int(np.mean(image_df.index)), int(np.mean(image_df.columns)) ]
+
 def build_image_df(image):
 	df = pd.DataFrame( index=range(len(image)), columns=range(len(image[0])) )
 	for i in df.index:
-		df.loc[i] = [ (r,g,b,a) for r,g,b,a in image[i] ]
+		df.loc[i] = [ (r,g,b) for r,g,b,a in image[i] ]
 	return df
+
+def recalc_pixels(image_df, ):
+	for i in image_df.index:
+		for j in image_df.columns:
+
 
 if __name__ == '__main__':
 	main()
